d2f1d859e5f5ef435fc7a7a1dab016805c6baa0a
dkibogy <jepkemboikibogy5@gmail.com>
2020-05-14 16:04:26 +0300

    pop up button created and the jquery function for button is added

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,5 @@
 // what we do section
-   $(document).ready(function(){
-        $("#WhatWeDo").click(function(){
-            $(this.#toggleDesignLogo).toggle();
-        });
-    });
+   
 // porfolio
 
 $(".card").hover(function(){
